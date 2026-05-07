15ff03c909a68da499f880a76208f6b273e1c58f
santiagoluisicloud-ship-it <santiagoluisicloud@icloud.com>
2026-05-07 07:01:33 -0500

    Update UV config to change bare URL

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://student-government.xyz",
+  bare: "https://tomp.app",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
